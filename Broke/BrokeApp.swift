e2d983702c5bdc766adc216c3bbab4a4c062e427
Wilson <30482728+wilson-makDev@users.noreply.github.com>
2023-09-08 14:17:19 -0400

    Initial Commit

---
+++ Broke/BrokeApp.swift
@@ -0,0 +1,20 @@
+//
+//  BrokeApp.swift
+//  Broke
+//
+//  Created by Wilson Mak on 2023-09-08.
+//
+
+import SwiftUI
+
+@main
+struct BrokeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
